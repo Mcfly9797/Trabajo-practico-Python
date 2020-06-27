0fd3f7e54c42336984d85a27ac57679ed204caa7
mcfly9797 <nahuelsvillarreal@gmail.com>
2020-06-27 05:38:37 -0300

    Initial commit

---
+++ Trabajo practico.py
@@ -0,0 +1,41 @@
+
+#Validacion de valores positivos, en este caso solo se tendra en cuenta este tipo de validacion
+def es_positivo(valor)
+    flag = False
+    if valor >= 0:                                                                         #Verifico que el valor no sea negativo
+        flag=True
+
+    return Flag
+
+#Datos generales semanales
+input dolar
+valido
+input precio girasol
+valido
+input precio soja
+valido
+
+
+
+inicializo lista de buques
+
+mientras el id buque ingresado no sea nulo entro a la funcion
+    #Datos por buque seran solicitados hasta que el buque ingresado este "" (vacion)
+    input idbuque
+    valido id no repetido recorriendo lista de buques
+    input tipo_cereal
+    valido si es "soja" "girasol"
+    input peso
+    valido que sea positivo
+    input calidad
+    valido que tenga un rango entre 0.5 y 1
+
+inicializo acumuladores
+calculo costo operativo
+realizo comparaciones de maximos solicitado
+
+
+Al finalizar carga semanal imprimo los datos solicitados
+
+
+
