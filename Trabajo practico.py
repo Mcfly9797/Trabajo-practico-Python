52e9e02d788243f71d0d930b398f949a41549a31
mcfly9797 <nahuelsvillarreal@gmail.com>
2020-06-27 05:56:08 -0300

    Agrego funcion "Ingreso_dat_buque". Recibe lista de los buques y id del buque actual a ingresar. Solicita datos: tipo_cereal, peso, calidad. Valida los datos y devuelve un nodo con todos los datos de un buque(id, tipo_cereal, peso, calidad)

--- Trabajo practico.py
+++ Trabajo practico.py
@@ -7,7 +7,7 @@ def valido_id(id,lista_buques)
             flag =True
     return flag
 
-
+##Valido que cereal sea soja o girasol
 def valido_cereal(tipo_cereal):
     flag = False
         if tipo_cereal == "Soja" or tipo_cereal == "Girasol"                                #Valido si el valor ingresado no coincide con los tipos de cereales 
@@ -22,6 +22,7 @@ def es_positivo(valor)
 
     return Flag
 
+##Valido que la calidad este entre 0.5 y 1
 def valido_calidad(calidad):                                                                #Recibo un valor
     flag = False
     if calidad >= 0.5 and calidad <= 1:                                                     #Valido que el numero ingresado sea mayor que cero.
@@ -29,6 +30,38 @@ def valido_calidad(calidad):                                                    
 
     return flag
 
+
+##Funcion que recepciona y orquesta la validacion de los datos ingresados, el valor id se ingresa previamente para validar si es nulo antes de ingresar a esta funcion
+def ingreso_dat_buque(lista_buques,id_buque):
+
+    while !valido_id_repetido(id_buque,lista_buques):                                       #Valido que no se repita id
+        id_buque = str(input("Ingreso un buque repetido, vuelva a intentar:"))              
+    
+
+    tipo_cereal = str(input("Escriba 'Soja' o 'Girasol' dependiendo del tipo de cereal:"))  
+    while !valido_cereal(tipo_cereal)                                                       #Valido que sea del tipo solicitado
+        tipo_cereal = str(input("Ingreso valor erroneo, vuelva a intentar:"))               
+    
+
+    
+    
+    peso = int(input("Ingrese el peso del cereal en kg: "))
+    while !valido_positivo(peso)                                                            #Valido que el peso sea un valor positivo
+        peso = int(input("Ingreso valor negativo. Vuelva a intentar: "))    
+    
+    
+    
+    
+    calidad = float(input("Ingrese calidad de cereales entre 0.5 y 1: "))
+    while !valido_calidad(calidad)                                                          #Valido que el rango de calidad sea correcto
+        calidad = int(input("Ingreso calidad erronea. Vuelva a intentar: "))    
+
+
+
+
+    nodo_buque = id_buque,tipo_cereal,peso,calidad                                          #Una vez validados todos los datos, creo un nodo y lo devuelvo para agregarlo a la lista
+
+    return nodo_buque
 
 #Datos generales semanales
 input dolar
